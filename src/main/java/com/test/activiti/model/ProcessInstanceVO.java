1ee62fd8891367154bb7d1d45a0c44e33edff2a8
wuzf <wuzhangfeng@rys.com>
2020-07-16 19:07:01 +0800

    在线编辑器

---
+++ src/main/java/com/test/activiti/model/ProcessInstanceVO.java
@@ -0,0 +1,15 @@
+package com.test.activiti.model;
+
+import lombok.Data;
+
+/**
+ * @author lipo
+ * @date 2020/7/15
+ */
+@Data
+public class ProcessInstanceVO {
+    private String id;
+    private String processDefinitionName;
+    private String processDefinitionKey;
+    private Integer processDefinitionVersion;
+}
